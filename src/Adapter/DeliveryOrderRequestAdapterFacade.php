fb9634fc84a31e23b695bb833d2111fd16cfc8ea
Adam Zagroba <adam.zagroba@paysera.net>
2025-08-06 16:26:15 +0200

    Cs fixer fix

--- src/Adapter/DeliveryOrderRequestAdapterFacade.php
+++ src/Adapter/DeliveryOrderRequestAdapterFacade.php
@@ -17,7 +17,6 @@ class DeliveryOrderRequestAdapterFacade
         OrderCreateRequestAdapter $createAdapter,
         OrderUpdateRequestAdapter $updateAdapter
     ) {
-
         $this->createAdapter = $createAdapter;
         $this->updateAdapter = $updateAdapter;
     }
