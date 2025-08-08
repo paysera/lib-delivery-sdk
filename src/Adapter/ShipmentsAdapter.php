6286d455408933abc5580162918782faab0ec139
Adam Zagroba <adam.zagroba@paysera.net>
2025-08-08 09:47:17 +0200

    Code review fix

--- src/Adapter/ShipmentsAdapter.php
+++ src/Adapter/ShipmentsAdapter.php
@@ -7,7 +7,6 @@ namespace Paysera\DeliverySdk\Adapter;
 use Paysera\DeliveryApi\MerchantClient\Entity\ShipmentCreate;
 use Paysera\DeliverySdk\Collection\OrderItemsCollection;
 use Paysera\DeliverySdk\Entity\MerchantOrderItemInterface;
-use Paysera\DeliverySdk\Entity\PayseraDeliverySettingsInterface;
 
 class ShipmentsAdapter
 {
@@ -31,16 +30,16 @@ class ShipmentsAdapter
      */
     private function createSingleShipment(OrderItemsCollection $items): iterable
     {
-        $width = 0.0;
-        $height = 0.0;
-        $length = 0.0;
-        $weight = 0.0;
+        $width = 0;
+        $height = 0;
+        $length = 0;
+        $weight = 0;
 
         foreach ($items as $item) {
-            $width += floor($item->getWidth());
-            $height += floor($item->getHeight());
-            $length += floor($item->getLength());
-            $weight += floor($item->getWeight());
+            $width += ceil($item->getWidth());
+            $height += ceil($item->getHeight());
+            $length += ceil($item->getLength());
+            $weight += ceil($item->getWeight());
         }
 
         return [
